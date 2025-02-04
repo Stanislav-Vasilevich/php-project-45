60cd0974a5425c8b96ae87c2bb4df4d06245d598
StanislaVailevich <stanislavasilevich@yandex.ru>
2025-02-04 21:39:03 +0300

    add namespace in file Cli.php and function run, import function run in file brain-games and write path, add autoload src/Cli.php in composer.json

---
+++ src/Cli.php
@@ -0,0 +1,12 @@
+<?php
+  
+  namespace phpProject45\Cli;
+  
+  use function cli\line;
+  use function cli\prompt;
+  
+  function run() {
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+  }
